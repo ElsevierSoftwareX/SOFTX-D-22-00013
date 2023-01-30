f25cad75d0fb1d4437ad8afebe405b2d55961fde
masaoAshtine <masao.ashtine@oerc.ox.ac.uk>
2023-01-30 00:42:18 +0000

    Creating of SoftwareX Repository

---
+++ ReorderData.m
@@ -0,0 +1,71 @@
+load('SmArchTest1_MeasurementResults.mat')
+alphaAll = alphaAll(:,end:-1:1)./2;
+alphaAll_std = alphaAll_std(:,end:-1:1)./2;
+deltaAll_std = delatAll_std(:,end:-1:1);
+clear delatAll_std
+deltaAll = deltaAll(:,end:-1:1);
+IDLengthsAvg1 = IDLengthsAvg1(:,end:-1:1);
+IDLengthsAvg1_std = IDLengthsAvg1_std(:,end:-1:1);
+IDLengthsAvg2 = IDLengthsAvg2(:,end:-1:1);
+IDLengthsAvg2_std = IDLengthsAvg2_std(:,end:-1:1);
+save('SmArchTest1_MeasurementResults.mat')
+
+load('SmArchTest2_MeasurementResults.mat')
+alphaAll = alphaAll(:,end:-1:1)./2;
+alphaAll_std = alphaAll_std(:,end:-1:1)./2;
+deltaAll_std = delatAll_std(:,end:-1:1);
+clear delatAll_std
+deltaAll = deltaAll(:,end:-1:1);
+IDLengthsAvg1 = IDLengthsAvg1(:,end:-1:1);
+IDLengthsAvg1_std = IDLengthsAvg1_std(:,end:-1:1);
+IDLengthsAvg2 = IDLengthsAvg2(:,end:-1:1);
+IDLengthsAvg2_std = IDLengthsAvg2_std(:,end:-1:1);
+save('SmArchTest2_MeasurementResults.mat')
+
+load('SmArchTest3_MeasurementResults.mat')
+alphaAll = alphaAll(:,end:-1:1)./2;
+alphaAll_std = alphaAll_std(:,end:-1:1)./2;
+deltaAll_std = delatAll_std(:,end:-1:1);
+clear delatAll_std
+deltaAll = deltaAll(:,end:-1:1);
+IDLengthsAvg1 = IDLengthsAvg1(:,end:-1:1);
+IDLengthsAvg1_std = IDLengthsAvg1_std(:,end:-1:1);
+IDLengthsAvg2 = IDLengthsAvg2(:,end:-1:1);
+IDLengthsAvg2_std = IDLengthsAvg2_std(:,end:-1:1);
+save('SmArchTest3_MeasurementResults.mat')
+
+load('SmArchTest4_MeasurementResults.mat')
+alphaAll = alphaAll(:,end:-1:1)./2;
+alphaAll_std = alphaAll_std(:,end:-1:1)./2;
+deltaAll_std = delatAll_std(:,end:-1:1);
+clear delatAll_std
+deltaAll = deltaAll(:,end:-1:1);
+IDLengthsAvg1 = IDLengthsAvg1(:,end:-1:1);
+IDLengthsAvg1_std = IDLengthsAvg1_std(:,end:-1:1);
+IDLengthsAvg2 = IDLengthsAvg2(:,end:-1:1);
+IDLengthsAvg2_std = IDLengthsAvg2_std(:,end:-1:1);
+save('SmArchTest4_MeasurementResults.mat')
+
+load('SmArchTest5_MeasurementResults.mat')
+alphaAll = alphaAll(:,end:-1:1)./2;
+alphaAll_std = alphaAll_std(:,end:-1:1)./2;
+deltaAll_std = delatAll_std(:,end:-1:1);
+clear delatAll_std
+deltaAll = deltaAll(:,end:-1:1);
+IDLengthsAvg1 = IDLengthsAvg1(:,end:-1:1);
+IDLengthsAvg1_std = IDLengthsAvg1_std(:,end:-1:1);
+IDLengthsAvg2 = IDLengthsAvg2(:,end:-1:1);
+IDLengthsAvg2_std = IDLengthsAvg2_std(:,end:-1:1);
+save('SmArchTest5_MeasurementResults.mat')
+
+load('SmArchTest6_MeasurementResults.mat')
+alphaAll = alphaAll(:,end:-1:1)./2;
+alphaAll_std = alphaAll_std(:,end:-1:1)./2;
+deltaAll_std = delatAll_std(:,end:-1:1);
+clear delatAll_std
+deltaAll = deltaAll(:,end:-1:1);
+IDLengthsAvg1 = IDLengthsAvg1(:,end:-1:1);
+IDLengthsAvg1_std = IDLengthsAvg1_std(:,end:-1:1);
+IDLengthsAvg2 = IDLengthsAvg2(:,end:-1:1);
+IDLengthsAvg2_std = IDLengthsAvg2_std(:,end:-1:1);
+save('SmArchTest6_MeasurementResults.mat')
